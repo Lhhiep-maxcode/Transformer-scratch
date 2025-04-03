b90bc7a92994e842d7fdef322be2f21470eae0e8
Lhhiep-maxcode <22028295@vnu.edu.vn>
2025-04-03 15:10:28 +0700

    complete training loop

---
+++ config.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import re
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 20,
+        "lr": 1e-3,
+        "seq_len": 150,
+        "d_model": 512,
+        "lang_src": "vi",
+        "lang_tgt": "en",
+        "preload": "latest",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+def get_weights_file_path(config, epoch: int):
+    model_folder = config['model_folder']
+    model_basename = config['model_basename']
+    model_filename = f"{model_basename}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
+
+def latest_weights_file_path(config):
+    model_folder = config['model_folder']
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    
+    if len(weights_files) == 0:
+        return None
+    
+    # Extract epoch number from filename using regex
+    def extract_epoch(file):
+        match = re.search(r'(\d+)', file.stem)  # Extract digits
+        return int(match.group(1)) if match else -1  # Convert to int
+    
+    # Sort based on epoch number
+    weights_files.sort(key=extract_epoch)
+
+    return str(weights_files[-1])  # Return latest model
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import warnings
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -7,6 +8,10 @@ from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from dataset import BilingualDataset, causal_mask
 from torch.utils.data import Dataset, DataLoader, random_split
+from model import build_transformer
+from torch.utils.tensorboard import SummaryWriter
+from config import get_config, get_weights_file_path, latest_weights_file_path
+from tqdm import tqdm
 
 
 from pathlib import Path
@@ -76,16 +81,113 @@ def get_ds(config):
     
     config_seq_len = config['seq_len']
     max_found_seq_len = max(max_len_src, max_len_tgt)
-    seq_len = max(config_seq_len, max_found_seq_len)
 
     if config_seq_len < max_found_seq_len:
-        print(f"Override value provided for seq_len parameter to {seq_len}")
+        raise Exception(f"Max founded sequence length is {max_found_seq_len}, but config seq_len is {config_seq_len}. Please increase the config seq_len.")
     
-    train_ds = BilingualDataset(ds_raw_train, tokenizer_src, tokenizer_tgt, seq_len)
-    val_ds = BilingualDataset(ds_raw_valid, tokenizer_src, tokenizer_tgt, seq_len)
+    train_ds = BilingualDataset(ds_raw_train, tokenizer_src, tokenizer_tgt, config['seq_len'])
+    val_ds = BilingualDataset(ds_raw_valid, tokenizer_src, tokenizer_tgt, config['seq_len'])
 
     train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
     val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
 
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
     
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(src_vocab_size=vocab_src_len, tgt_vocab_size=vocab_tgt_len,
+                              src_seq_len=config['seq_len'], tgt_seq_len=config['seq_len'],
+                              d_model=config['d_model'])
+    return model
+
+
+def train_model(config):
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print("Using device:", device)
+    if (device == 'cuda'):
+        print(f"Device name: {torch.cuda.get_device_name(device.index)}")
+        print(f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
+    elif (device == 'mps'):
+        print(f"Device name: <mps>")
+    else:
+        print("NOTE: If you have a GPU, consider using it for training.")
+        print("      On a Windows machine with NVidia GPU, check this video: https://www.youtube.com/watch?v=GMSjDTU8Zlc")
+        print("      On a Mac machine, run: pip3 install --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cpu")
+    device = torch.device(device)
+
+
+    Path(f"{config['model_folder']}").mkdir(parents=True, exist_ok=True)
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+    # Tensorboard
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    # If the user specified a model to preload before training, load it
+    initial_epoch = 0
+    global_step = 0
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch')
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch:04d}")
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
+            decoder_input = batch['decoder_input'].to(device) # (B, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, seq_len, seq_len)
+
+            # Run the tensors through the encoder, decoder and the projection layer
+            encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
+            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+
+            # Compare the output with the label
+            label = batch['label'].to(device) # (B, seq_len)
+
+            # Compute the loss using a simple cross entropy
+            # loss((B * seq_len, vocab_size), (B * seq_len))
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            # Log the loss
+            writer.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            # Backpropagate the loss
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step += 1
+
+        # Save the model at the end of every epoch
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step
+        }, model_filename)
+    
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
