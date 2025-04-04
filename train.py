0b462983b2fe4632da2750c62a0bbd044187e954
Lhhiep-maxcode <22028295@vnu.edu.vn>
2025-04-04 19:28:39 +0700

    fix bug wrong passing order of params

--- train.py
+++ train.py
@@ -346,7 +346,7 @@ def train_model(config):
 
             global_step += 1
 
-        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config["seq_len"], device, lambda msg: batch_iterator.write(msg), global_step, config, writer)
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config["seq_len"], device, lambda msg: batch_iterator.write(msg), global_step, writer, config)
 
         # Save the model at the end of every epoch
         model_filename = get_weights_file_path(config, f"{epoch:02d}")
